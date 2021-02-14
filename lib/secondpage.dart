475b939b859843511a97f75ff6fbc00632fcf82c
Mozarts-Requiem-in-D-Minor <57464583+Mozarts-Requiem-in-D-Minor@users.noreply.github.com>
2021-02-14 11:59:14 -0500

    updated shared preference second page

--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:Login/main.dart';
 
-
-class SecondPage extends StatelessWidget{
-  Widget build(BuildContext context){
-    return new  Scaffold(
-        appBar: AppBar(
-          title: Text("Second Page")));
-        
+class SecondPage extends StatefulWidget{
+  String username = "";
+  @override
+  _SecondPageState createState() => new _SecondPageState();
   }
 
+  
+class _SecondPageState extends State<SecondPage>{
+  String _username = "";
+  @override
+  void initState() {
+    getNamePreferences().then(updateUsername);
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+      title: new Text("Second Page"),
+      ),
+      body: new ListTile(title: new Text(_username),)
+      );
+  }
+  void updateUsername(String username){
+    
+    setState((){
+      this._username = username;
+    });
+  }
 
 }
